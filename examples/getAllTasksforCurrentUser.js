832b616e5f95439d6394b20dba44a265a44988ba
Fredrik Mundt <fredrik@mundt.no>
2017-03-07 11:25:55 +0100

    Setup examples with bearer token

--- examples/getAllTasksforCurrentUser.js
+++ examples/getAllTasksforCurrentUser.js
@@ -3,6 +3,6 @@ var client = new Wrike({
   bearer_token: process.env.WRIKE_BEARER_TOKEN
 });
 
-client.get('contacts',{me: true}, function(error,data,response){
+client.get('tasks', function(error,data,response){
   console.log(data);
 });
